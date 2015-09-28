bacc17dc6c3015eae5ac3dedfea9c82658534dda
GBoh <greg@bohanon3d.com>
2015-09-28 10:47:51 -0500

    base models created

---
+++ Shiftr/Shiftr/Models/Schedule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shiftr.Models {
+    public class Schedule {
+        public int Id { get; set; }
+        public IList<Shift> Shifts { get; set; }
+
+    }
+}
---
+++ Shiftr/Shiftr/Models/Shift.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shiftr.Models {
+    public class Shift {
+        public int Id { get; set; }
+        public DateTime StartShift { get; set; }
+        public DateTime EndShift { get; set; }
+        public ApplicationUser Employee { get; set; }
+
+    }
+}
